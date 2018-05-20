dd5403e0e8c81b9390eb7e6b4c9f7f3aaac4cbcc
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-05-20 13:25:51 +0200

    ICO Coming Soon Landing Page for discoverblockchain.io - final preparations before website going live

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@
     /*----------------------------------------------------*/
     /* Final Countdown Settings
     ------------------------------------------------------ */
-    var finalDate = '2018/05/20';
+    var finalDate = '2018/05/21';
     $('div#counter').countdown(finalDate)
         .on('update.countdown', function (event) {
             $(this).html(event.strftime('<div class=\"half\">' +
